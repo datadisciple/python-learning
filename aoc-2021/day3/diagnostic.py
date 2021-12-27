1f060948513a9021f5434c87df324e7709b6ffdb
datadisciple <sean@sparkgrills.com>
2021-12-26 20:22:26 -0700

    adds day3-[binary diangnostic]-pt2

--- aoc-2021/day3/diagnostic.py
+++ aoc-2021/day3/diagnostic.py
@@ -1,8 +1,3 @@
-filename = "report.txt"
-
-with open(filename, 'r') as report:
-    binary_list = report.read().splitlines()
-
 def calc_gamma(list_of_codes):
     total_chars = len(list_of_codes[0])
     index_val = []
@@ -32,10 +27,60 @@ def calc_gamma(list_of_codes):
 
     return gamma_rate
 
+
 def calc_epsilon(gamma_rate):
     epsilon_rate = gamma_rate.replace('1', 'X').replace('0', '1').replace('X', '0')
 
     return epsilon_rate
+
+
+def calc_rating(list_of_codes, rating_type = 'o2'):
+    rating_code = [list_of_codes[i] for i in range(len(list_of_codes))]
+        # initialize new list based of the values in list passed in
+        # without doing this, the original list will be overwritten bc of pop() — (function would only work correctly the first time it is called)
+    total_chars = len(list_of_codes[0])
+
+    for position in range(total_chars):
+        most_common = 0
+
+        for code in rating_code:
+            char = code[position]            
+            if char == '1':
+                most_common += 1
+            else:
+                most_common -= 1
+        
+        if rating_type == 'o2':
+
+            if most_common > 0: criteria = '1'
+            elif most_common == 0: criteria = '1'
+            else: criteria = '0'
+
+        elif rating_type == 'co2':
+
+            if most_common > 0: criteria = '0'
+            elif most_common == 0: criteria = '0'
+            else: criteria = '1'
+        
+        else:
+            raise Exception(f"incorrect rating_type; accepted_value: 'o2' or 'co2'")
+
+        index = 0
+
+        while len(rating_code) > 1 and index < len(rating_code):
+            code = rating_code[index]
+            char = code[position]
+
+            if char != criteria:
+                rating_code.pop(index)
+            else:
+                index += 1
+
+    rating = ''
+    rating = rating.join(rating_code)    
+    
+    return rating
+
 
 def main():
     filename = "report.txt"
@@ -52,6 +97,18 @@ def main():
     print(f"gamma rate: {gamma_rate} = {gmag}")
     print(f"epsilon rate: {epsilon_rate} = {emag}")
     print(f"total power consumption: {consumption}")
+
+    o2_rating = calc_rating(binary_list)
+    o2_val = int(o2_rating, 2)
+    co2_rating = calc_rating(binary_list, 'co2')
+    co2_val = int(co2_rating, 2)
+
+    life_rating = o2_val * co2_val
+
+    print(f"o2_rating: {o2_rating} = {o2_val}")
+    print(f"co2_rating: {co2_rating} = {co2_val}")
+    print(f"life rating = {life_rating}")   
+
 
 if __name__ == "__main__":
     main()
